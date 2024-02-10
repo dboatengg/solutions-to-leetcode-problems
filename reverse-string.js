19bb98bf0f233d2cf769421e85d6791d17c4eb9d
dboatengg <dicksonboateng67@gmail.com>
2024-02-10 01:28:55 +0000

    Optimize code for reverse-string problem

--- reverse-string.js
+++ reverse-string.js
@@ -4,15 +4,25 @@
 // 'world'  =>  'dlrow'
 // 'word'   =>  'drow'
 
-//=============================================Code
+//=============================================Previous Code
+// function reverseString(str) {
+//   const stringArray = str.split('');
+//   const revereStringArray = stringArray.reverse();
+//   const combinedString = revereStringArray.join('');
+//   return combinedString;
+// }
+
+//============================================= Optimized Code
 function reverseString(str) {
-  const stringArray = str.split('');
-  const revereStringArray = stringArray.reverse();
-  const combinedString = revereStringArray.join('');
-  return combinedString;
+  return str.split('').reverse().join('');
 }
 
 //=============================================Code Explanation
+//The approach implement in the optimized code is called method chaining
+//The 'split' method divides the string into an array of characters
+//The 'reverse' method reverses the order of items in the array
+//Lastly, the 'join' method joins array of string characters back into a single string
 
 //=============================================Function call
 console.log(reverseString('colour'));
+console.log(reverseString('dcba'));
