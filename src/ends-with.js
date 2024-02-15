608d52670cda03072d4c8948b4f40c8015e4360c
dboatengg <dicksonboateng67@gmail.com>
2024-02-15 12:02:30 +0000

    Complete endsWith problem

---
+++ src/ends-with.js
@@ -0,0 +1,6 @@
+//=========================================Instructions
+//Complete the solution so that it returns true if the...
+//first argument(string) passed ends with the 2nd argument(also a string).
+function endsWith(str, ending) {
+  return str.endsWith(ending);
+}
