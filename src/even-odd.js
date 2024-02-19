4654d31f08c037b75ea3ec985a18a227bfea8f4c
dboatengg <dicksonboateng67@gmail.com>
2024-02-19 13:35:10 +0000

    finish even-or-odd problem

---
+++ src/even-odd.js
@@ -0,0 +1,8 @@
+//======================================instructions
+//Create a function that takes an integer as an argument and
+//returns "Even" for even numbers or "Odd" for odd numbers.
+
+//======================================my solution
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
